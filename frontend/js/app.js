89aff97ec0e0954696c16ce230950dc2942e9039
Chawengwit <chawengwit@taximail.com>
2025-08-27 12:02:55 +0700

    Sets up basic frontend structure
    Initializes the frontend with a basic HTML structure,
    SPA routing and placeholder components.
    
    This commit sets the stage for future feature implementations.

---
+++ frontend/js/app.js
@@ -0,0 +1,5 @@
+// SPA page loader & router
+import { renderHome } from './pages/home.js';
+import { renderLogin } from './pages/login.js';
+import { renderAdmin } from './pages/admin.js';
+import { renderNotFound } from './pages/404.js';
