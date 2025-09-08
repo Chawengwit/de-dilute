1776639f41c8f63f2a77cb1dbeae208c826a44e0
Chawengwit <chawengwit@taximail.com>
2025-09-08 17:26:56 +0700

    clean log

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,6 @@ import pool from './db.js'; // PostgreSQL connection pool
 
 // Load environment variables dynamically based on NODE_ENV
 const envPath = `../.env.${process.env.NODE_ENV || "development"}`;
-console.log("Loading environment variables from:", envPath);
 dotenv.config({ path: envPath });
 
 const PORT = process.env.BACKEND_PORT || 3000;
@@ -16,9 +15,7 @@ async function startServer() {
     console.log("✅ Database connected successfully");
 
     app.listen(PORT, () => {
-      console.log(
-        `🚀 Server is running on port ${PORT} in ${process.env.NODE_ENV} mode.`
-      );
+      console.log(`🚀 Server is running`);
     });
   } catch (err) {
     console.error("❌ Failed to connect to the database:", err);
